1c9656c43fb82f272e620bee14e146130ccf14a4
DESKTOP-2UCLB32\weblazer <will.blazer@gmail.com>
2018-04-15 23:56:32 -0400

    Added new empty page TestBed1 for experimenting with Ionic controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { TestBed1 } from '../pages/testbed1/testbed1';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +23,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'TestBed1', component: TestBed1 }
     ];
 
   }
---
+++ src/pages/testbed1/testbed1.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'testbed1',
+  templateUrl: 'testbed1.html'
+})
+export class TestBed1 {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
